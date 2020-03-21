b0d593b6af3d593e7de96b42905387c46189d72c
rachele <rachele.terzoli@gmail.com>
2020-03-21 13:58:37 +0100

    esercizio due finito

--- ex2/main.js
+++ ex2/main.js
@@ -1,19 +1,14 @@
 alert("ora ti chiederò di inserire due parola e ti stamperò la più lunga. premi ok per inserire le due parole.");
-var str = prompt("inserisci la prima parola:");
-var paroladue = prompt("inserisci la seconda parola:");
-var uno = parolauno.lenght;
- document.getElementById('stampa').innerHTML = uno;
-
-if (parolauno > paroladue) {
-}
-    document.getElementById('stampa').innerHTML = 'La parola ' + parolauno + ' è più lunga!';
-    document.getElementById('stampa').innerHTML = 'La parola ' + paroladue + ' è più corta!';
+var parolauno = prompt("inserisci la prima parola");
+var paroladue = prompt("inserisci la seconda parola");
+if (parolauno.length < paroladue.length) {
+document.getElementById("stampa").innerHTML = "La parola " + parolauno + " é più corta ed è lunga " + parolauno.length + " caratteri, invece " + paroladue + " è più lunga ed è lunga " + paroladue.length + " caratteri" ;
 
 }
-else if (paroladue > parolauno){
-  document.getElementById('stampa').innerHTML = 'La parola ' + paroladue + ' è più lunga!';
-  document.getElementById('stampa').innerHTML = 'La parola ' + parolauno + ' è più corta!';
+ else if (paroladue.length < parolauno.length )  {
+document.getElementById("stampa").innerHTML = "La parola " + paroladue + " é più corta ed è lunga " + paroladue.length + " caratteri, invece " + parolauno + " è più lunga ed è lunga " + parolauno.length + " caratteri" ;
+
 }
-else if (paroladue = parolauno){
-  document.getElementById('stampa').innerHTML = 'Avete inserito due parole della stessa lunghezza.';
+else {
+   document.getElementById("stampa").innerHTML = "Le parole hanno le stessa lunghezza. Sono lunghe " + parolauno.length + " caratteri" ;
 }
